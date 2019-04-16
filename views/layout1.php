b521032c1349871d7c5b8ed3298fbf71a22334d3
ghena shaban <ghena@hotmail.co.uk>
2019-04-16 16:26:51 +0100

    update`

---
+++ views/layout1.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Travel Blog</title>
+	<link rel="stylesheet" href="bootstrap-4.1.3-dist/css/bootstrap.min.css">
+        
+	<link rel="stylesheet" href="css/fixed.css">
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+</head>
+<body data-spy="scroll" data-target="#navbarResponsive">
+    
+<!--- start home section --->
+<div id="home">
+ <!-- start of nav -->
+ <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+ <a class="navbar-brand" href="#">Logo</a>
+ <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+     <span class="navbar-toggler-icon"></span>   
+ </button>
+ <!-- change href to controller/action and remove divs -->
+ <div class="collapse navbar-collapse" id="navbarResponsive">
+     <ul class="navbar-nav ml-auto">
+         <li class="nav-item">
+             <a class="nav-link" href="#Home">Home</a>
+             </li>
+             <li class="nav-item">
+             <a class="nav-link" href="#destination">Destination</a>
+             </li>
+             <li class="nav-item">
+             <a class="nav-link" href="#ecoTravel">Eco Travel</a>
+             </li>
+             <li class="nav-item">
+             <a class="nav-link" href="#familyTravel">Family Travel</a>
+             </li>
+             <li class="nav-item">
+             <a class="nav-link" href="#inspiration">Inspiration</a>
+             </li>
+             <li class="nav-item">
+             <a class="nav-link" href="#practicalTips">Practical Tips</a>
+             </li>
+         
+     </ul> 
+ </div>
+ </nav>
+ <!-- end of nav -->   
+ <!-- start landing page -->
+ <div class="landing">
+     <div class="home-wrap">
+         <div class="home-inner">
+             
+         </div>
+     </div>  
+ </div>
+ <div class="caption text-center">
+     <h1>Welcome to our Travel Blog</h1>
+     <h3>Start your journey today</h3>
+     <a class="btn btn-outline-light btn-lg" href="#">
+         Sign up/ get started
+     </a>
+ </div>
+ 
+ <!-- end landing page -->
+ 
+ 
+</div>
+
+<!--- End home section --->
+<!--- start destination section --->
+<div id="destination" class="offset">
+    
+</div>
+
+<!--- End destination section --->
+<!--- start eco travel section --->
+<div id="ecoTravel" class="offset">
+    
+</div>
+
+<!--- End eco travel section --->
+<!--- start family travel section --->
+<div id="familyTravel" class="offset">
+    
+</div>
+
+<!--- End family travel section --->
+<!--- start inspiration section --->
+<div id="inspiration" class="offset">
+    
+</div>
+
+<!--- End inspiration section --->
+<!--- start practical tips section --->
+<div id="practicalTips" class="offset">
+    
+</div>
+
+<!--- End practical tips section --->
+
+<!--- Script Source Files -->
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
+<script src="https://use.fontawesome.com/releases/v5.5.0/js/all.js"></script>
+<!--- End of Script Source Files -->
+
+</body>
+</html>
+
+
+
