2c86b859dd119f0c286692de759a84369a58f872
MelanieLeather <35497730+MelanieLeather@users.noreply.github.com>
2019-04-17 18:02:27 +0100

    css/layout update

--- views/layout1.php
+++ views/layout1.php
@@ -5,14 +5,15 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Travel Blog</title>
 	<link rel="stylesheet" href="bootstrap-4.1.3-dist/css/bootstrap.min.css">
-        
-	<link rel="stylesheet" href="css/fixed.css">
         <link href="css/style.css" rel="stylesheet" type="text/css"/>
+	<link rel="stylesheet" href="css/fixed.css">        
 </head>
+
 <body data-spy="scroll" data-target="#navbarResponsive">
     
 <!--- start home section --->
 <div id="home">
+    
  <!-- start of nav -->
  <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
  <a class="navbar-brand" href="#">Logo</a>
@@ -39,12 +40,12 @@
              </li>
              <li class="nav-item">
              <a class="nav-link" href="#practicalTips">Practical Tips</a>
-             </li>
-         
+             </li>         
      </ul> 
  </div>
  </nav>
- <!-- end of nav -->   
+ <!-- end of nav -->  
+ 
  <!-- start landing page -->
  <div class="landing">
      <div class="home-wrap">
@@ -53,6 +54,7 @@
          </div>
      </div>  
  </div>
+ 
  <div class="caption text-center">
      <h1>Welcome to our Travel Blog</h1>
      <h3>Start your journey today</h3>
@@ -62,41 +64,231 @@
  </div>
  
  <!-- end landing page -->
- 
- 
+
 </div>
 
 <!--- End home section --->
+
 <!--- start destination section --->
+
 <div id="destination" class="offset">
     
+    <div class="col-12 narrow text-center">
+        <h1>Here are our latest destination articles</h1>
+        <p class="lead">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+        <a class="btn btn-secondary btn-md" href="#">Find out more</a>
+</div>    
 </div>
-
 <!--- End destination section --->
-<!--- start eco travel section --->
-<div id="ecoTravel" class="offset">
-    
-</div>
 
+<!--- start eco travel section --->
+<div id="ecoTravel" class="offset">    
+    
+    <div class="jumbotron">
+        <div class="narrow text-center">
+            
+            <div class="col-12">
+                <h3 class="heading">EcoTravel</h3>
+                <div class="heading-underline"></div>
+        </div>   
+            
+            <div class="row">                
+                <div class="col-md-4">
+                    <div class="feature">
+                         <i class="fas fa-play-circle fa-4x" data-fa-transform="shrink-3 up-5"></i>
+                         <h3>Custom Animation</h3>
+                         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>                       
+                     </div>                      
+        </div>
+                
+            <div class="col-md-4">
+                <div class="feature">
+                    <i class="fas fa-sliders-h fa-4x" data-fa-transform="shrink-4.5 up-4.5"></i>
+                        <h3>Content slider</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>                       
+                     </div>                      
+        </div>
+            
+            <div class="col-md-4">
+                     <div class="feature">
+                         <i class="fab fa-wpforms fa-4x" data-fa-transform="shrink-4 up-5"></i>
+                         <h3>Contact form</h3>
+                         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>                       
+                     </div>                      
+        </div>
+            
+</div>             
+</div>
+</div>
+</div>
 <!--- End eco travel section --->
+
 <!--- start family travel section --->
 <div id="familyTravel" class="offset">
-    
+
+    <div class="fixed-background">
+        
+        <div class="row dark text-center"> 
+        
+        <div class="col-12">
+                <h3 class="heading">Family travel</h3>
+                <div class="heading-underline"></div>
+        </div>  
+        
+        <div class="col-md-4">
+            <h3>More on family travel</h3>
+                <div class="feature">
+                         <i class="fas fa-code fa-3x"></i>
+                </div>
+                    <p class="lead">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>                       
+                     </div>
+            
+            <div class="col-md-4">
+            <h3>More on family travel</h3>
+                    <div class="feature">
+                         <i class="fas fa-bold fa-3x"></i>
+                    </div>
+                    <p class="lead">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>                       
+                     </div>  
+            
+            <div class="col-md-4">
+            <h3>More on family travel</h3>
+                    <div class="feature">
+                         <i class="fab fa-css3 fa-3x"></i>
+                    </div>
+                        <p class="lead">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>                       
+                    </div>  
+        </div>
+
+     <div class="fixed-wrap">
+         <div class="fixed">
+             
+         </div>
+     </div>  
+ </div>  
 </div>
 
 <!--- End family travel section --->
+
 <!--- start inspiration section --->
 <div id="inspiration" class="offset">
+  
+    <div class="jumbotron">
     
+           <div class="col-12 text-center">
+                <h3 class="heading">Inspiration</h3>
+                <div class="heading-underline"></div>
+        </div>  
+        
+        <div class="row">
+            <div class="col-md-6 clients">
+                <div class="row">
+                <div class="col-md-4">
+                    <img src="images/client1.png">
+                </div>
+                    <div class="col-md-8">
+                        <blockquote>
+                            <i class="fas fa-quote-left"></i>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor
+                            <hr class="clients-hr">
+                            <cite>&#8212; Some dummy text</cite>
+                        </blockquote>
+    </div>
+                            </div>
 </div>
+ 
+        <div class="col-md-6 clients">
+                <div class="row">
+                <div class="col-md-4">
+                    <img src="images/client1.png">
+                </div>
+                    <div class="col-md-8">
+                        <blockquote>
+                            <i class="fas fa-quote-left"></i>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor
+                            <hr class="clients-hr">
+                            <cite>&#8212; Some dummy text</cite>
+                        </blockquote>
 
-<!--- End inspiration section --->
-<!--- start practical tips section --->
-<div id="practicalTips" class="offset">
-    
+    </div>
+                            </div>
+
 </div>
+</div> 
+</div>
+    
+    <div class="col-12 narrow text-center">
+        <p class="lead">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor</p>
+        <a class="btn btn-secondary btn-md" href="#">Find out more</a>
+    </div>
+    </div>
+<!--- End inspiration section --->
+
+<!--- start practical tips section --->
+<div id="practicalTips" class="offset">    
+    
+    <div class="jumbotron">
+        <div class="narrow text-center">
+            
+            <div class="col-12">
+                <h3 class="heading">Practical tips</h3>
+                <div class="heading-underline"></div>
+        </div>   
+            
+            <div class="row">
+                
+                 <div class="col-md-4">
+                     <div class="feature">
+                         <i class="fas fa-play-circle fa-4x" data-fa-transform="shrink-3 up-5"></i>
+                         <h3>Custom Animation</h3>
+                         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>                       
+                     </div>                      
+        </div>
+                
+                 <div class="col-md-4">
+                     <div class="feature">
+                         <i class="fas fa-sliders-h fa-4x" data-fa-transform="shrink-4.5 up-4.5"></i>
+                         <h3>Content slider</h3>
+                         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>                       
+                     </div>                      
+        </div>
+            
+          <div class="col-md-4">
+             <div class="feature">
+                <i class="fab fa-wpforms fa-4x" data-fa-transform="shrink-4 up-5"></i>
+                   <h3>Contact form</h3>
+                    p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>                       
+                     </div>                      
+        </div>
+            
+</div>             
+</div>
+</div>
+</div> 
+
 
 <!--- End practical tips section --->
+
+<!-- Footer -->
+<div id="contact" class="offset">
+<footer>
+<div class="row justify-content-center">
+    <div class="col-md-5 text-center">
+      <!--image to be added <img src="">-->
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p> 
+        <strong>Contact info  </strong>
+        <p>99999<br>email address</p>
+        <a href="#" target="_blank"><i class="fab fa-facebook-square"></i></a>
+        <a href="#" target="_blank"><i class="fab fa-twitter-square"></i></a>
+        <a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
+    </div>
+    
+    <hr class="socket">
+    &copy; Blog theme
+</div>
+</footer>
+</div>
+<!-- End of footer -->
 
 <!--- Script Source Files -->
 <script src="js/jquery-3.3.1.min.js"></script>
@@ -106,6 +298,3 @@
 
 </body>
 </html>
-
-
-
